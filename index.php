2a764747387ce73f23a25425e1c73c62c7bd4d63
Henri Larget <henri.larget@aboutgoods.net>
2020-10-01 23:22:44 +0200

    adding details on cv

--- index.php
+++ index.php
@@ -7,11 +7,22 @@ $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates/');
 $twig = new \Twig\Environment($loader, [
     //'cache' => __DIR__."/cache/",
 ]);
+$locale = "en";
+$translations_filename = "locale_" . $locale . ".json";
+$translations = json_decode(@file_get_contents($translations_filename), true) ?? [];
 $twig->addFilter(new \Twig\TwigFilter("md5", "md5"));
-$parsedown = new Parsedown();
-$twig->addFilter(new \Twig\TwigFilter("md", [$parsedown, "text"]));
+$twig->addFilter(new \Twig\TwigFilter("md", [new Parsedown(), "text"]));
+$twig->addFilter(new \Twig\TwigFilter("t", function ($message, $arguments = []) use (&$translations) {
+    if (!isset($translations[$message])) {
+        $translations[$message] = "";
+    }
+    return sprintf($translations[$message], ...$arguments);
+
+
+}));
 $value = \Symfony\Component\Yaml\Yaml::parseFile("app.yaml");
 $details = \Symfony\Component\Yaml\Yaml::parseFile("details.yaml");
 
 $template = $twig->load('index.html.twig');
 echo $template->render(array_merge(["details" => $details], $value));
+file_put_contents($translations_filename, json_encode($translations ?? [], JSON_PRETTY_PRINT));
